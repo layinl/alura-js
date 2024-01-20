b68af412ddbe5044b436e00e374c066363fa2629
layinl <layon.costa@protonmail.com>
2024-01-20 01:09:13 -0300

    feat: add 3rd exercise pages and add loop in the main app

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 alert("Welcome to the Secret Number Game!");
 
-let secretNumber = 2;
-let number = prompt("Choose a number from 1 to 10");
+let secretNumber = 7;
+let counter = 1;
+let number;
 
-if (number == secretNumber) alert(`Nice! You discovered the secret number (${secretNumber})`);
-else alert("Too bad. You missed...");
+while (counter <= 5) {
+	number = prompt("Choose a number from 1 to 10");
+	if (number == secretNumber) {
+		alert(`Nice! You discovered the secret number (${secretNumber})`);
+		break;
+	}
+	else if (number < secretNumber)
+		alert("The secret number is greater than " + number);
+	else
+		alert("The secret number is less than " + number);
+	counter++;
+}
