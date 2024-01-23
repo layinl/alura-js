85cadb142aa77b7be3a033cbd7e917af4c3c3f89
layinl <55414324+layinl@users.noreply.github.com>
2024-01-23 20:19:39 +0000

    feat: add text to html through js and create function

---
+++ alura-js-2/app.js
@@ -0,0 +1,8 @@
+let title = document.querySelector("h1");
+title.innerHTML = "Welcome to the Number Guessing Game!";
+let paragraph = document.querySelector("p");
+paragraph.innerHTML = "Guess the number";
+
+function verifyGuess() {
+    console.log("working...");
+}
