974eed92202495388b45b875a0bcf247743bab92
layinl <layon.costa@protonmail.com>
2024-01-22 23:12:47 -0300

    feat: randomize secret number and spice up the alert

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 alert("Welcome to the Secret Number Game!");
 
-let secretNumber = 7;
+let secretNumber = parseInt(Math.random() * 10) + 1;
 let counter = 1;
-let number;
+let number = -1;
 
-while (counter <= 5) {
+console.log(secretNumber);
+while (secretNumber != number) {
 	number = prompt("Choose a number from 1 to 10");
-	if (number == secretNumber) {
-		alert(`Nice! You discovered the secret number (${secretNumber})`);
+	if (number == secretNumber)
 		break;
-	}
 	else if (number < secretNumber)
 		alert("The secret number is greater than " + number);
 	else
 		alert("The secret number is less than " + number);
 	counter++;
 }
+
+alert(`Nice! You discovered the secret number (${secretNumber}) in ${counter} ${(counter > 1)? "tries." : "try."}`);
