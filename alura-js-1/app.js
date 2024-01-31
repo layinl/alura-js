354339eaa03f98904154434477db7b1bede48193
layinl <55414324+layinl@users.noreply.github.com>
2024-01-31 17:23:29 +0000

    feat: add links to home page on each main project

--- alura-js-1/app.js
+++ alura-js-1/app.js
@@ -7,14 +7,18 @@ let number = -1;
 
 console.log(secretNumber);
 while (secretNumber != number) {
-	number = prompt(`Choose a number from 1 to ${MAX_NUMBER}`);
-	if (number == secretNumber)
+	number = prompt(`Choose a number from 1 to ${MAX_NUMBER}. \n Type back to return to index`);
+	if (number == "back") {
+		history.back();
 		break;
+	}
+	if (number == secretNumber) {
+		alert(`Nice! You discovered the secret number (${secretNumber}) in ${counter} ${(counter > 1)? "tries." : "try."}`);
+		break;
+	}
 	else if (number < secretNumber)
 		alert("The secret number is greater than " + number);
 	else
 		alert("The secret number is less than " + number);
 	counter++;
 }
-
-alert(`Nice! You discovered the secret number (${secretNumber}) in ${counter} ${(counter > 1)? "tries." : "try."}`);
